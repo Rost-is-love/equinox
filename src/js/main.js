af9e9e49743b385ed8c4798d7d968baa3f6c9683
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2020-12-18 14:18:20 +0300

    create get os

--- src/js/main.js
+++ src/js/main.js
@@ -16,54 +16,94 @@ const gamePopup = document.querySelectorAll('.game-popup');
 const closePopup = document.querySelectorAll('.game-popup__close');
 const body = document.body;
 
-const addPopup = (popup) => {
-  popup.classList.add('popup-open');       
-}
+const popupWork = () => {
+  const addPopup = (popup) => {
+    popup.classList.add('popup-open');       
+  }
 
-const removePopup = () => {
-  gamePopup.forEach((e) => {
-    e.classList.remove('popup-open');   
-  })     
-}
+  const removePopup = () => {
+    gamePopup.forEach((e) => {
+      e.classList.remove('popup-open');   
+    })     
+  }
 
-const disableScroll = () => {
-  const paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
-  body.style.paddingRight = paddingOffset;
-  body.classList.add('disable-scroll');  
-}
+  const disableScroll = () => {
+    const paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
+    body.style.paddingRight = paddingOffset;
+    body.classList.add('disable-scroll');  
+  }
 
-const enableScroll = () => {
-  body.classList.remove('disable-scroll');
-  body.style.paddingRight = '0px';
-}
+  const enableScroll = () => {
+    body.classList.remove('disable-scroll');
+    body.style.paddingRight = '0px';
+  }
 
-astrofall.addEventListener('click', () => {
-  addPopup(astrofallPopup);
-  disableScroll();
-});
-
-spacetor.addEventListener('click', () => {
-  addPopup(spacetorPopup);
-  disableScroll();
-});
-
-hayseed.addEventListener('click', () => {
-  addPopup(hayseedPopup);
-  disableScroll();
-});
-
-closePopup.forEach((el) => {
-  el.addEventListener('click', () => {
-    removePopup();
-    enableScroll();
+  astrofall.addEventListener('click', () => {
+    addPopup(astrofallPopup);
+    disableScroll();
   });
-});
 
-gamePopup.forEach((el) => {
-  el.addEventListener('click', (e) => {
-    if (e.target === el) {
-      removePopup(el);
+  spacetor.addEventListener('click', () => {
+    addPopup(spacetorPopup);
+    disableScroll();
+  });
+
+  hayseed.addEventListener('click', () => {
+    addPopup(hayseedPopup);
+    disableScroll();
+  });
+
+  closePopup.forEach((el) => {
+    el.addEventListener('click', () => {
+      removePopup();
       enableScroll();
-    }
+    });
   });
-});
+
+  gamePopup.forEach((el) => {
+    el.addEventListener('click', (e) => {
+      if (e.target === el) {
+        removePopup(el);
+        enableScroll();
+      }
+    });
+  });
+};
+
+const linksToAndroid = () => {
+  astrofall.setAttribute('href', 'https://play.google.com/store/apps/details?id=com.MagicPieStudio.Astrofall');
+  spacetor.setAttribute('href', 'https://play.google.com/store/apps/details?id=com.EquinoX.Spacetor');
+  hayseed.setAttribute('href', 'https://play.google.com/store/apps/details?id=com.EquinoX.Hayseed');
+};
+
+const linksToIos = () => {
+  astrofall.setAttribute('href', 'https://apps.apple.com/us/app/astrofall/id1384150110');
+  spacetor.setAttribute('href', 'https://apps.apple.com/us/app/spacetor/id1494466119');
+  hayseed.setAttribute('href', 'https://apps.apple.com/ru/app/hayseed/id1495236916');
+};
+
+const getMobileOperatingSystem = () => {
+  let userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if (/android/i.test(userAgent)) {
+      return "Android";
+  }
+
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return "iOS";
+  }
+
+  return "unknown";
+}
+
+const detectAndServe = () => {
+  let os = getMobileOperatingSystem();
+  if (os === "Android") {
+    linksToAndroid();
+  } else if (os === "iOS") {
+    linksToIos();
+  } else {
+    popupWork(); 
+  }
+}
+detectAndServe();
